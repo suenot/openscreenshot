d5a4705de3ff5c1f1920575072668c7f122cf30c
Eugen Soloviov <suenot@gmail.com>
2026-04-06 16:48:06 +0300

    Initial Commit

---
+++ OpenScreenshot/OpenScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenScreenshotApp.swift
+//  OpenScreenshot
+//
+//  Created by Eugen Soloviov on 06/04/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
